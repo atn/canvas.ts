c72953f0045f1ef64abc9bd0a2410de566b11b86
atn <austin@aust.nyc>
2021-04-11 16:56:13 -0400

    Update basic.ts

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,10 +1,9 @@
-import { Client } from '../lib'
+const { Client } = require('./lib')
 const client = new Client()
 
+// when client connects to Canvas
 client.on('connect', async () => {
-  console.log(client.user)
-  console.log(await client.todo.get())
-  client.courses.get('x')
+  console.log(`Logged into ${client.domain} as ${client.user.name}, Enrolled in ${client.courses.size} courses.`)
 })
 
-client.connect('xxxx', 'yyy.example.com') 
+client.connect('auth', 'domain.example.com')
